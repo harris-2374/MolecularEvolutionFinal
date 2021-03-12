3b2ff57a36f0af20e74d8abb57ba00b9b1625566
harris-2374 <41405364+harris-2374@users.noreply.github.com>
2021-03-11 21:57:27 -0600

    Gene lookup
    - ensemblGeneFamilyHomologyFileParser.py only records data where the provided gene is found, allowing for easier access to the data.

--- ensemblGeneFamilyHomologyFileParser.py
+++ ensemblGeneFamilyHomologyFileParser.py
@@ -9,7 +9,6 @@ from pathlib import Path
 
 import pandas as pd
 
-
 ########################## Helper Functions ##########################
 def writeTreeFile(data, filename):
     with open(filename, 'w') as oh:
@@ -32,10 +31,10 @@ def getFileChunks(INPUT, fileChunkOutput):
     key value is a list of lines from the file """
     currentChunk = 1
     with open(INPUT) as fh:
-        phyloGroups = fh.read().split("//\n")
-        for count, group in enumerate(phyloGroups, 1):
+        geneGroups = fh.read().split("//\n")
+        for count, group in enumerate(geneGroups, 1):
             if count % 10 == 0:
-                print(f"-- {count:,}/{len(phyloGroups):,} --")
+                print(f"-- {count:,}/{len(geneGroups):,} --")
             seqlines = [l for l in group.split("\n") if l != '']
             currChunkDir = fileChunkOutput / f"chunk_{currentChunk}"
             currChunkDir.mkdir(parents=True, exist_ok=True)  # Make output directory
@@ -61,7 +60,7 @@ def getFileChunks(INPUT, fileChunkOutput):
                     except ValueError:
                         print(seqdata)
                         raise ValueError
-                    df.to_csv(currChunkSeqFile, sep="\t", index=False)
+                    # df.to_csv(currChunkSeqFile, sep="\t", index=False)
                     break
                 # else:
                 #     break
@@ -84,6 +83,14 @@ def main():
         help="",
     )
     parser.add_argument(
+        '-g',
+        '--INPUT_GENE',
+        type=str,
+        action='store',
+        required=True,
+        help="Comma separated list of INPUT_GENEs to search for [Replace spaces with underscores]",
+    )
+    parser.add_argument(
         '-o',
         '--output',
         type=str,
@@ -98,6 +105,7 @@ def main():
     # by the user to something we can use and 
     # manipulate in our code.
     INPUT = Path(args.input)
+    INPUT_GENE = args.INPUT_GENE
     OUTPUT = Path(args.output)
     OUTPUT.mkdir(parents=True, exist_ok=True)
 
@@ -105,51 +113,53 @@ def main():
     Input file structure: (Compara.102.protein_default.nh)
         - 'SEQ' data
         - 'DATA' header
-        - Newick phylogenetic tree
+        - Newick phyloINPUT_GENEtic tree
         - '//' to indicate break to next set of data
         - Blank line
         - Next set of 'SEQ' data...
     """
-
     # Step 1: Parse file into chunks
-    fileChunkOutput = OUTPUT / 'fileChunks'
+    fileChunkOutput = OUTPUT / f'{INPUT_GENE}'
     currentChunk = 1
     with open(INPUT) as fh:
-        phyloGroups = fh.read().split("//\n")
-        for count, group in enumerate(phyloGroups, 1):
-            if count % 10 == 0:
-                print(f"-- {count:,}/{len(phyloGroups):,} --")
+        geneGroups = fh.read().split("//\n")
+        for count, group in enumerate(geneGroups, 1):
             seqlines = [l for l in group.split("\n") if l != '']
-            currChunkDir = fileChunkOutput / f"chunk_{currentChunk}"
-            currChunkDir.mkdir(parents=True, exist_ok=True)  # Make output directory
-            currChunkSeqFile = currChunkDir / f"chunk_{currentChunk}_SEQ.tsv"
-            currChunkTreeFile = currChunkDir / f"chunk_{currentChunk}_Newick.tree"
             for i, l in enumerate(reversed(seqlines), 1):
                 if not l: # Skips blank lines
                     continue
-                elif ';' in l:  # Write out newick file
-                    writeTreeFile(l, currChunkTreeFile)
+                elif ';' in l:
+                    tree = l
                     continue
                 elif "SEQ" in l:
                     seqdata = [l.split(" ") for l in seqlines[:len(seqlines)-i]]
-                    avglen = sum([len(i) for i in seqdata]) / len(seqdata)
-                    print(avglen, max([len(i) for i in seqdata]), min([len(i) for i in seqdata]))
-
+                    # This section fixes an issue where INPUT_GENEs have
+                    # spaces in their names at the end of the row
+                    # This joins the name by replacing the space
+                    # with an underscore 
                     for l in seqdata:
                         if len(l) > 9:
-                            print(l)
-
+                            l[8:len(l)+1] = ['_'.join(l[8:len(l)+1])] 
                     try:
-                        df = pd.DataFrame(data=seqdata, columns=range(len(seqdata[0])))
-                    except ValueError:
-                        # print(seqdata)
-                        raise ValueError
-                    df.to_csv(currChunkSeqFile, sep="\t", index=False)
+                        df = pd.DataFrame(data=seqdata, columns=['SEQ', 'Species', 'ProteinID', 'Chromosome', 'Start', 'Stop', 'gain-loss?', 'GeneID', 'Gene'])
+                    except:
+                        print(seqdata)
+                    df['Gene'] = df['Gene'].apply(lambda x: 'NULL' if not x else x) # Replace None with "NULL", None throws error
+                    nullCount = df['Gene'].to_list().count('NULL')
+                    for n, gene in enumerate(df['Gene']):
+                        if INPUT_GENE in gene:
+                            currChunkDir = fileChunkOutput / f"chunk_{currentChunk}"
+                            currChunkDir.mkdir(parents=True, exist_ok=True)  # Make output directory
+                            currChunkSeqFile = currChunkDir / f"chunk_{currentChunk}_SEQ.tsv"
+                            currChunkTreeFile = currChunkDir / f"chunk_{currentChunk}_Newick.tree"
+                            df.to_csv(currChunkSeqFile, sep="\t", index=False)
+                            writeTreeFile(tree, currChunkTreeFile)
+                            currentChunk += 1
+                            break
                     break
-            currentChunk += 1
+            if count % 1000 == 0:
+                print(f"-- {count:,}/{len(geneGroups):,} --")
             continue
-
-
     return
 
 if __name__ == "__main__":
