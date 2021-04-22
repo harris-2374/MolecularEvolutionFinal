0bc921bf9cb694dc7008a0aedb88d56ede806b23
harris-2374 <41405364+harris-2374@users.noreply.github.com>
2021-04-22 16:43:39 -0500

    update scripts

---
+++ scripts/fetchEnsemblSequences.py
@@ -0,0 +1,138 @@
+"""
+Author: Andrew Harris
+Python 3.8 
+
+Dependency install commands
+conda install ensembl-rest or pip install ensembl-rest
+conda install pandas or pip install pandas
+"""
+import argparse
+import logging
+import os
+from pathlib import Path
+import textwrap
+
+## Dependencies
+import ensembl_rest
+import pandas as pd
+
+########################### Main Function ###########################
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument(
+        '-i',
+        '--input',
+        type=str,
+        action='store',
+        required=True,
+        default='ensembl_gene_lookup_input.tsv',
+        help="Excel/tsv file with gene name, common name, GeneID, and ProteinID",
+    )
+    parser.add_argument(
+        '-g',
+        '--gene',
+        type=str,
+        action='store',
+        required=True,
+        default='ensembl_gene_lookup_input.tsv',
+        help="Ensembl gene name to look up",
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        action='store',
+        default='./ensembl_sequences',
+        help='',
+    )
+    args = parser.parse_args()
+    
+    # --- Input Argparse Variables ---
+    INPUT= Path(args.input)
+    GENE = str(args.gene)
+    OUTPUT= Path(args.output)
+    GENE_OUTPUT = OUTPUT / 'GeneID' / GENE
+    PROTEIN_OUTPUT = OUTPUT / 'ProteinID' / GENE
+    GENE_LOG_OUTPUT = OUTPUT / 'gene_logs/'
+    PROTEIN_LOG_OUTPUT = OUTPUT / 'protein_logs/'
+    GENE_LOG_OUTPUT_FILE = GENE_LOG_OUTPUT / f'{GENE}_fetchEnsemblSequences_GeneID.log'
+    PROTEIN_LOG_OUTPUT_FILE = PROTEIN_LOG_OUTPUT / f'{GENE}_fetchEnsemblSequences_ProteinID.log'
+
+    # Clear log file if it exist
+    if os.path.exists(GENE_LOG_OUTPUT_FILE):
+        os.remove(GENE_LOG_OUTPUT_FILE)
+    if os.path.exists(PROTEIN_LOG_OUTPUT_FILE):
+        os.remove(PROTEIN_LOG_OUTPUT_FILE)
+
+    # Make output directory
+    GENE_OUTPUT.mkdir(parents=True, exist_ok=True)
+    GENE_LOG_OUTPUT.mkdir(parents=True, exist_ok=True)
+    PROTEIN_OUTPUT.mkdir(parents=True, exist_ok=True)
+    PROTEIN_LOG_OUTPUT.mkdir(parents=True, exist_ok=True)
+
+    # Set up logging output files + handlers + streams
+    formatter = logging.Formatter('%(message)s')
+    # Gene logging
+    gene_logger = logging.getLogger(GENE_LOG_OUTPUT_FILE.as_posix())
+    gene_logger.setLevel(logging.INFO)
+    gene_stream_handler = logging.StreamHandler()
+    gene_file_handler = logging.FileHandler(GENE_LOG_OUTPUT_FILE)
+    gene_file_handler.setFormatter(formatter)
+    gene_logger.addHandler(gene_file_handler)
+    gene_logger.addHandler(gene_stream_handler)
+    # Protein logging
+    protein_logger = logging.getLogger(PROTEIN_LOG_OUTPUT_FILE.as_posix())
+    protein_logger.setLevel(logging.INFO)
+    protein_stream_handler = logging.StreamHandler()
+    protein_file_handler = logging.FileHandler(PROTEIN_LOG_OUTPUT_FILE)
+    protein_file_handler.setFormatter(formatter)
+    protein_logger.addHandler(protein_file_handler)
+    protein_logger.addHandler(protein_stream_handler)
+
+    # Load input file into df + fill na values with str(NULL)
+    df = pd.read_csv(INPUT, sep='\t')
+    df = df.fillna('NULL')
+    # Fetch information + output + log
+    for row in df.itertuples(index=False):
+        if 'NULL' in row.Gene:
+            geneName = 'NULL_Novel'
+        else:
+            geneName = row.Gene
+        # Row attributes
+        commonName = row.CommonName
+        sampleOrder = row.Order
+        geneID = row.GeneID
+        proteinID = row.ProteinID
+        # Output filenames
+        geneOutputFileName = GENE_OUTPUT / f"{geneName}_{commonName}_{sampleOrder}_{geneID}.fasta"
+        proteinOutputFileName = PROTEIN_OUTPUT / f"{geneName}_{commonName}_{sampleOrder}_{proteinID}.fasta"
+        # Fetch gene sequences
+        try:
+            gene_fasta_results = ensembl_rest.sequence_id(geneID)
+            with open(geneOutputFileName, 'w') as oh:
+                oh.write(f">{geneOutputFileName.stem}\n")
+                oh.write(textwrap.fill(gene_fasta_results['seq'], width=80))
+            gene_logger.info('----------------------------------------------------------------')
+            gene_logger.info(f"CommonName: {commonName}\nGeneID: {geneID}\nSeqFile: {geneOutputFileName}\nDescription: {gene_fasta_results['desc']}")
+        except:
+            gene_logger.info('----------------------------------------------------------------')
+            gene_logger.info(f'Could not find info for {geneID}')
+            pass
+        # Fetch protein sequences
+        try:
+            protein_fasta_results = ensembl_rest.sequence_id(proteinID)
+            with open(proteinOutputFileName, 'w') as oh:
+                oh.write(f">{proteinOutputFileName.stem}\n")
+                oh.write(textwrap.fill(protein_fasta_results['seq'], width=80))
+            protein_logger.info('----------------------------------------------------------------')
+            protein_logger.info(f"CommonName: {commonName}\nProteinID: {proteinID}\nSeqFile: {proteinOutputFileName}\nDescription: {protein_fasta_results['desc']}")
+        except:
+            protein_logger.info('----------------------------------------------------------------')
+            protein_logger.info(f'Could not find info for {proteinID}')
+            pass
+        continue
+    return
+
+if __name__ == "__main__":
+    main()
+    
---
+++ scripts/speciesHomologyParser.py
@@ -0,0 +1,184 @@
+"""
+Author: Andrew Harris
+Python 3.8
+Version: 0.0.1
+Last update: 2-23-2021
+"""
+import argparse
+import logging
+import os
+from pathlib import Path
+## Dependencies
+import pandas as pd
+
+"""
+    SOI = Species of interest
+
+    Outline for species homology file:
+        1. Load files into pandas dataframeframes
+        2. Filter dataframeframe (see filter section below)
+        3. Group dataframe by 'gene_stable_id'
+        4. Search for genes from genes-of-interest file
+        5. Output information with or w/out all speices (only keep species-of-interest if SOI file provided)
+
+    Filters:
+        1. Percent identity >= 50%
+        2. 'homology_type' == 'ortholog_one2many' or 'ortholog_one2one'
+        3. 
+"""
+
+########################### Logging Init ###########################
+# If log file exist, erase it
+if os.path.exists('logs/speciesHomologyParser.log'):
+    os.remove('logs/speciesHomologyParser.log')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)s: %(message)s')
+Path("logs/").mkdir(parents=True, exist_ok=True)
+file_handler = logging.FileHandler('logs/speciesHomologyParser.log')
+stream_handler = logging.StreamHandler()
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
+########################## Helper Functions ##########################
+
+
+
+########################### Main Function ###########################
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument(
+        '-i',
+        '--input',
+        type=str,
+        action='store',
+        required=True,
+        help="Homology file path",
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        action='store',
+        default='./',
+        help='',
+    )
+    parser.add_argument(
+        '-s',
+        '--species',
+        type=str,
+        action='store',
+        default=None,
+        help='Species of interest list (single species name per line -- basic txt file)',
+    )
+    parser.add_argument(
+        '-g',
+        '--genes',
+        type=str,
+        action='store',
+        default=None,
+        help='Gene of interest',
+    )
+    parser.add_argument(
+        '-p',
+        type=int,
+        action='store',
+        default=50,
+        help='Percent identity cutoff',
+    )
+    args = parser.parse_args()
+    
+    # --- Input Argparse Variables ---
+    INPUT = Path(args.input)
+    OUTPUT = Path(args.output)
+    SPECIES = args.species
+    GENES = Path(args.genes)
+    PERCENTID = int(args.p)
+    logger.info(f"File Name: {INPUT.name}")
+
+    OUTPUT.mkdir(parents=True, exist_ok=True)
+
+    # Step 1: Load dataframe into dataframeframes
+    # sep='\t' tells pandas it is a tab delimited file (tsv)
+    df = pd.read_csv(INPUT, sep='\t')
+    geneDF = pd.read_csv(GENES, names=['geneID', 'geneName'], sep='\t')
+    logger.info(f'Number of genes to search for: {len(geneDF)}')
+    try:
+        species_path = Path(SPECIES)
+        if 'csv' in species_path.name:
+            speciesDF = pd.read_csv(species_path, sep='\t')
+        elif 'xls' in species_path.name:
+            speciesDF = pd.read_excel(species_path, engine='openpyxl')
+    except TypeError:
+        speciesDF = None
+
+    # Step 2: Filter dataframeFrame
+    length_before_filter = len(df)
+    
+    one2many = df[(df['identity'] >= PERCENTID) & (df['homology_type'] == 'ortholog_one2many')]
+    one2one = df[(df['identity'] >= PERCENTID) & (df['homology_type'] == 'ortholog_one2one')]
+    
+    filtered_df = pd.concat([one2one, one2many])
+    
+    logger.info(f"Intial number of entries: {length_before_filter:,}")
+    logger.info(f"Remaining number of entries after filter: {len(filtered_df):,} ({len(filtered_df)-length_before_filter:,})")
+
+    # Step 3: Group dataframe by 'gene_stable_id'
+    grouped_df = filtered_df.groupby(by=['gene_stable_id'])
+    logger.info(f"Number of genes: {len(grouped_df):,}")
+    
+    # Step 4: Find genes of interest and output dataframeframe
+    # to a file.
+    for gene, dataframe in grouped_df:
+        if gene in geneDF['geneID'].to_list():
+            geneINFO = geneDF[geneDF['geneID'] == gene]  # Remove all dataframeframe entries besides the current gene of interest
+            geneINFO = geneINFO.reset_index(drop=True)   # Reset index so gene is at index position 0
+            assert len(geneINFO) == 1  # Make sure gene is not added to list twice
+            geneName = geneINFO['geneName'][0]
+            outFilename = OUTPUT / f'{geneName}_{gene}_results.tsv'
+            if not SPECIES:
+                # Output dataframe with all dataframe
+                logger.info("===========================================")
+                logger.info(f"Gene name: {gene}")
+                logger.info(f"Number of entries: {len(dataframe)}")
+                logger.info(f"Number of unique species: {len(dataframe['homology_species'].unique())}")
+                logger.info(dataframe)
+                logger.info("===========================================")
+                # Write results to file
+                dataframe.to_csv(outFilename, sep='\t', index=False)
+            else:
+                # Remove non-species of interest dataframe
+                filteredDF = pd.DataFrame()
+                uniqueNames = dataframe['homology_species'].drop_duplicates()
+                namesToRemove = pd.concat([uniqueNames, speciesDF['scientific_name']])
+                namesToRemove = namesToRemove.drop_duplicates(keep=False)
+                for s in namesToRemove:
+                    if filteredDF.empty:
+                        filteredDF = dataframe[dataframe.homology_species != s]
+                    else:
+                        filteredDF = filteredDF[filteredDF.homology_species != s]
+                # Now add in common names
+                conversionDict = {i:j for i,j in zip(speciesDF['scientific_name'], speciesDF['common_name'])}
+                filteredDF['common_name'] = filteredDF['homology_species'].apply(lambda x: conversionDict[x])
+                logger.info("===========================================")
+                logger.info(f"Gene name: {gene}")
+                logger.info(f"Number of unique species: {len(dataframe['homology_species'].unique())}")
+                logger.info(f"Number of entries after filtering: {len(filteredDF)}")
+                logger.info(filteredDF)
+                logger.info("===========================================")
+                filteredDF.to_csv(outFilename, sep='\t', index=False)
+    # Exit script by removing output directory if it is empty
+    # If output directory is not empty, just exit the script
+    outputFiles = [f for f in OUTPUT.iterdir()]
+    if len(outputFiles) > 0:
+        return
+    else:
+        OUTPUT.rmdir()
+        return
+
+if __name__ == "__main__":
+    main()
+
